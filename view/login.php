09db43ef170ec2103919b50d6e942059bfd43798
Edinei <galera.org@gmail.com>
2017-08-17 23:25:53 -0300

    style login set

--- view/login.php
+++ view/login.php
@@ -2,45 +2,47 @@
 if (VISITANTE) {
     $log = new \SessionControl\Login();
     ?>
-    <div class='container al-center font-size13 grey-text upper'>área restrita</div>
-    <div class="card container col l6 m10 s12 push-m1 push-l3 center-align pd-content30">
-        <div class="row">
-            <div class="input-field container">
-                <input id="emaillog" onkeyup="clickSend('loginbtn', event)" type="email" class="validate">
-                <label for="emaillog">Email</label>
+    <div class='container row pd-content60'>
+        <div class='container al-center font-size13 grey-text upper'>área restrita</div>
+        <div class="card container col l6 m10 s12 push-m1 push-l3 center-align pd-content30">
+            <div class="row">
+                <div class="input-field container">
+                    <input id="emaillog" onkeyup="clickSend('loginbtn', event)" type="email" class="validate">
+                    <label for="emaillog">Email</label>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="input-field container">
-                <input id="passlog" onkeyup="clickSend('loginbtn', event)" type="password" class="validate">
-                <label for="passlog">Senha</label>
+            <div class="row">
+                <div class="input-field container">
+                    <input id="passlog" onkeyup="clickSend('loginbtn', event)" type="password" class="validate">
+                    <label for="passlog">Senha</label>
+                </div>
             </div>
-        </div>
 
-        <?php if (defined("RECAPTCHASITE") && $log->checkAttemptsExceded()) { ?>
-            <div class="container">
-                <div class="g-recaptcha" data-sitekey="<?= RECAPTCHASITE ?>"></div>
-                <br>
-            </div>
-            <script type="text/javascript" src="https://www.google.com/recaptcha/api.js"></script>
-        <?php } else { ?>
+            <?php if (defined("RECAPTCHASITE") && $log->checkAttemptsExceded()) { ?>
+                <div class="container">
+                    <div class="g-recaptcha" data-sitekey="<?= RECAPTCHASITE ?>"></div>
+                    <br>
+                </div>
+                <script type="text/javascript" src="https://www.google.com/recaptcha/api.js"></script>
+            <?php } else { ?>
             <input type="hidden" id="g-recaptcha-response"/>
-        <?php } ?>
+            <?php } ?>
 
-        <div class="container pd-medium al-center" style="float:initial">
-            <a href="<?= defined('HOME') ? HOME : "" ?>cadastro-usuario"
-               class="waves-effect waves-teal btn-flat color-gray">Cadastre-se</a>
-            <button id="loginbtn" class="waves-effect waves-light btn" onclick="login();">
-                Entrar
-            </button>
+            <div class="container pd-medium al-center" style="float:initial">
+                <a href="<?= defined('HOME') ? HOME : "" ?>cadastro-usuario"
+                   class="waves-effect waves-teal btn-flat color-gray">Cadastre-se</a>
+                <button id="loginbtn" class="waves-effect waves-light btn" onclick="login();">
+                    Entrar
+                </button>
+            </div>
         </div>
-    </div>
 
-    <div class="row clearfix"></div>
+        <div class="row clearfix"></div>
 
-    <div class="container col l6 m10 s12 push-m1 push-l3 al-right">
-        <a href="<?= defined('HOME') ? HOME : "" ?>esqueci-a-senha"
-           class="upper font-size09 teal-text">esqueci a senha</a>
+        <div class="container col l6 m10 s12 push-m1 push-l3 al-right">
+            <a href="<?= defined('HOME') ? HOME : "" ?>esqueci-a-senha"
+               class="upper font-size09 teal-text">esqueci a senha</a>
+        </div>
     </div>
 
     <?php
