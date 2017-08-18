b1d43a26e31fd0a0810dfa2a03436bae7ddf048c
Edinei <galera.org@gmail.com>
2017-08-17 23:28:41 -0300

    esqueci a senha style

--- view/esqueci-a-senha.php
+++ view/esqueci-a-senha.php
@@ -1,20 +1,23 @@
-<div class="card container col l6 m10 s12 push-m1 push-l3 center-align pd-content30">
-    <div class="row">
-        <div class="input-field container">
-            <input id="recovery-email" onkeyup="clickSend('send-email-recover', event)" type="email" class="validate">
-            <label for="emaillog">Digite seu Email</label>
+<div class='container row pd-content60'>
+    <div class="card container col l6 m10 s12 push-m1 push-l3 center-align pd-content30">
+        <div class="row">
+            <div class="input-field container">
+                <input id="recovery-email" onkeyup="clickSend('send-email-recover', event)" type="email"
+                       class="validate">
+                <label for="emaillog">Digite seu Email</label>
+            </div>
+        </div>
+        <div class="container pd-medium al-center" style="float:initial">
+            <button class="waves-effect waves-light btn" id="send-email-recover" onclick="recoveryEmail();">
+                Enviar Email de Recuperação
+            </button>
         </div>
     </div>
-    <div class="container pd-medium al-center" style="float:initial">
-        <button class="waves-effect waves-light btn" id="send-email-recover" onclick="recoveryEmail();">
-            Enviar Email de Recuperação
-        </button>
+
+    <div class="row clearfix"></div>
+
+    <div class="container col l6 m10 s12 push-m1 push-l3 al-right">
+        <a href="<?= defined('HOME') ? HOME : "" ?>login" class="grey-text upper">voltar para
+            tela de login</a>
     </div>
-</div>
-
-<div class="row clearfix"></div>
-
-<div class="container col l6 m10 s12 push-m1 push-l3 al-right">
-    <a href="<?= defined('HOME') ? HOME : "" ?>login" class="grey-text upper">voltar para
-        tela de login</a>
 </div>
