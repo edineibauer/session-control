1182ba8d12ef827a0ea183c68956c389f33599e7
Edinei <galera.org@gmail.com>
2017-09-09 16:47:12 -0300

    cadastro implements with FormCrud

---
+++ view/cadastro-usuario.php
@@ -0,0 +1,15 @@
+<div class='container row pd-content60'>
+    <div class="card container col l6 m10 s12 push-m1 push-l3 center-align pd-content30">
+        <?php
+        $form = new FormCrud("user");
+        $form->showForm("materialize");
+        ?>
+    </div>
+
+    <div class="row clearfix"></div>
+
+    <div class="container col l6 m10 s12 push-m1 push-l3 al-right">
+        <a href="<?= defined('HOME') ? HOME : "" ?>login" class="grey-text upper">ir para
+            tela de login</a>
+    </div>
+</div>
