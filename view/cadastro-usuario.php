1b507341f2907647023d956703aa2cd9a27ca446
Edinei <galera.org@gmail.com>
2017-09-09 18:46:24 -0300

    tela cadastro updates

--- view/cadastro-usuario.php
+++ view/cadastro-usuario.php
@@ -1,4 +1,7 @@
 <div class='container row pd-content60'>
+
+    <div class='container center font-size13 grey-text upper'>Cadastro de novo Usuário</div>
+
     <div class="card col l8 m10 s12 push-m1 push-l2 center-align pd-content30">
         <div class="col s10 push-s1">
             <?php
