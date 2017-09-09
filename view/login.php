4cdb2facac57e74af32082db33c2a32576038f61
Edinei <galera.org@gmail.com>
2017-09-09 14:45:29 -0300

    update config file

--- view/login.php
+++ view/login.php
@@ -1,4 +1,19 @@
 <?php
+if(!defined("VISITANTE") && defined("PATH_HOME")) {
+
+    $conf = "\n\n\$session = new \SessionControl\Session();
+\$session->setLevelAccess(1, \"usuário\", \"user\",\"usuário do site, acesso a recursos e consumidor de produtos.\");
+\$session->setLevelAccess(2, \"produtor\", \"producer\", \"produtor de conteúdos para o site, ou abastecimento de informações.\");
+\$session->setLevelAccess(3, \"analista\", \"managment\", \"analista, verificador de conteúdo. Gerenciador dos usuários de produção e usuários do site\");
+\$session->setLevelAccess(4, \"gerente\", \"adm\", \"acesso total ao sistema com excessão ao gerenciamento de usuários de mesmo nível ou superior.\");
+\$session->setLevelAccess(5, \"administrador\", \"adm\", \"acesso total ao sistema e controle.\");\n\n";
+
+    $fp = fopen(PATH_HOME . "_config/config.php", "a+");
+    $escreve = fwrite($fp, $conf);
+    fclose($fp);
+
+    header("Location: login");
+}
 if (VISITANTE) {
     $log = new \SessionControl\Login();
     ?>
