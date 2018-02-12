87aebf9d5010a0e19ebb66cb87ed14742dce0f1c
Edinei <galera.org@gmail.com>
2018-02-12 16:55:21 -0200

    updatings

--- config.php
+++ config.php
@@ -2,33 +2,58 @@
 
 new LinkControl\EntityImport("session-control");
 
-$id = \Entity\Entity::add("login", ["nome" => "Admin", "nome_usuario" => "admin", "setor" => 1, "email" => EMAIL, "password" => ""]);
-
+$read = new \ConnCrud\Read();
+$read->exeRead(PRE . "login", "ORDER BY id ASC LIMIT 1");
+if (!$read->getResult())
+    $id = \Entity\Entity::add("login", ["nome" => "Admin", "nome_usuario" => "admin", "setor" => 1, "email" => EMAIL, "password" => "mudar"]);
+else
+    $id = $read->getResult()[0]['id'];
 ?>
-
+<style>body {
+        background: #eeeeee;
+    }</style>
 <div class="row">
     <div class="container">
-        <div class="card" style="background: #FFF; padding:30px; margin-top:20px; border-radius: 5px">
+        <div class="card"
+             style="background: #FFF; padding:30px; border-radius: 5px; float:initial; margin:30px auto; max-width: 900px">
             <br>
-            <h4>Criar Admin do Sistema</h4>
+            <h3 class="font-bold align-center upper">Administrador do Sistema</h3>
             <br>
+
+            <div class="card color-grey-light color-grey align-center">
+                <div class="font-medium padding-8 upper font-bold color-red">Atenção!</div>
+                <p class="padding-medium font-light">Atualize as informações de Administrador! Atualmente senha é: "<b>mudar</b>"
+                </p>
+            </div>
 
             <?php
             $form = new \FormCrud\Form("login");
             $form->showForm($id, ["nome", "nome_usuario", "email", "imagem", "password"]);
             ?>
 
-            <button onclick="loginArea()" class="waves-effect waves-light btn-large" id="btn-login" disabled="disabled"><i class="material-icons left">save</i>Login</button>
+            <button class="waves-effect waves-light btn-large" id="btn-login" disabled="disabled">
+                <i class="material-icons left padding-right">save</i><span class="left">Atualizar e fazer Login</span>
+            </button>
         </div>
     </div>
 </div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 
 <script>
-    $("input[data-model='dados.password']").on("change keyup", function () {
-        if($(this).val().length > 3)
-            $("#btn-login").prop("disabled", false).removeAttr("disabled");
-    });
-    function loginArea() {
-        post('config', 'configFinish', {lib:"session-control"}, function () {});
+    window.onload = function () {
+        $("input[data-model='dados.password']").on("change keyup", function () {
+            if ($(this).val().length > 3)
+                $("#btn-login").prop("disabled", false).addClass("color-green hover-shadow");
+        });
+
+        $("#btn-login").on("click", function () {
+            post('config', 'configFinish', {local: "session-control"}, function () {});
+        });
     }
+
 </script>
