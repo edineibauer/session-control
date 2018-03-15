a7fc41d2039f499e40182725a11eebac9a8eb063
Edinei <galera.org@gmail.com>
2018-03-15 16:34:36 -0300

    inserir senha nova

--- ajax/setNewPassword.php
+++ ajax/setNewPassword.php
@@ -1,28 +1,18 @@
 <?php
 use \ConnCrud\TableCrud;
+use Helpers\Check;
 
 $senha = strip_tags(trim(filter_input(INPUT_POST, 'senha', FILTER_DEFAULT)));
 $restoreCode = filter_input(INPUT_POST, 'code', FILTER_DEFAULT);
 
-function encrypt($senha)
-{
-    $senha = md5("Control" . trim($senha) . "Session");
-    $key1 = array('1', 'c', 's', '2', 'r', 'o', 'n', 'l', 'f', 'x', '0', 'k', 'v', '5', 'y');
-    $key2 = array('b', '4', '9', '6', 'w', 'a', 'd', '3', 'z', '7', 'j', 'm', '8', 'h', 't');
-    return md5(str_replace($key1, $key2, $senha));
-}
-
-$banco = new TableCrud("user_token");
-$banco->load("restore_code", $restoreCode);
+$banco = new TableCrud("login");
+$banco->load("token_recovery", $restoreCode);
 if ($banco->exist()) {
-    $banco->restore_code = "";
+    $banco->token_recovery = "";
     $banco->token = "";
-    $banco->expire = "";
-    $banco->password = encrypt($senha);
+    $banco->token_expira = "";
+    $banco->password = Check::password($senha);
     $banco->save();
 
-    echo 1;
-
-} else {
-    echo 2;
+    $data['data'] = 1;
 }
--- assets/inserir-nova-senha.js
+++ assets/inserir-nova-senha.js
@@ -4,26 +4,19 @@ function newPassword() {
     if (novaSenha) {
         novaSenha = false;
         if ($("#nova-senha").val() === $("#nova-senha-confirm").val()) {
-            $.post(HOME + 'request/post', {
-                lib: 'session-control',
-                file: 'setNewPassword',
-                senha: $("#nova-senha").val(),
-                code: $("#code").val()
-            }, function (g) {
+            post('session-control', 'setNewPassword', {senha: $("#nova-senha").val(), code: $("#code").val()}, function (g) {
                 if (g === "1") {
-                    Materialize.toast('Senha Modificada, redirecionando...', 2000);
+                    toast('Senha Modificada, redirecionando...', 1200);
                     setTimeout(function () {
                         window.location.href = HOME + "login";
-                    },2000);
-                } else if (g === "2") {
-                    Materialize.toast("Token de alteração Inválido! Favor, requisitar email de recuperação.", 6000);
+                    },1500);
                 } else {
-                    Materialize.toast("Erro Desconhecido", 3000);
-                    console.log(g);
+                    toast("Token Inválido! Tente recuperar senha novamente.", 6000, "warning");
                 }
                 novaSenha = true;
             });
         } else {
+            toast("senhas não correspondem", "warning");
             novaSenha = true;
         }
     }
