56c7bd893a9a225406ecfa3c0cc192b86c8ef299
Edinei <galera.org@gmail.com>
2018-05-21 22:25:28 -0300

    session new pattern

--- ajax/view/cadastro-usuario.php
+++ ajax/view/cadastro-usuario.php
@@ -1,25 +1,33 @@
-<div class='row font-large' style="max-width: 750px; margin: auto">
-    <div class="clear"><br></div>
-    <div class='container align-center upper panel font-light color-text-grey'>Cadastro de Usuário</div><br>
-    <div class="row z-depth-2 color-white">
-        <div class="panel">
+<?php
+ob_start();
+?>
+    <div class='row font-large' style="max-width: 750px; margin: auto">
+        <div class="clear"><br></div>
+        <div class='container align-center upper panel font-light color-text-grey'>Cadastro de Usuário</div>
+        <br>
+        <div class="row z-depth-2 color-white">
             <div class="panel">
-                <?php
-                $form = new \FormCrud\Form("usuarios");
-                $form->setAutoSave(false);
-                $form->setCallback("goToDashboard");
-                $form->showForm(["nome", "email", "password"]);
-                ?>
+                <div class="panel">
+                    <?php
+                    $form = new \FormCrud\Form("usuarios");
+                    $form->setAutoSave(false);
+                    $form->setCallback("goToDashboard");
+                    $form->showForm(["nome", "email", "password"]);
+                    ?>
+                </div>
             </div>
         </div>
+
+        <div class="row clear"><br></div>
+        <div class="row upper color-text-grey font-small">
+            <a href="<?= defined('HOME') ? HOME : "" ?>login"
+               class="left btn color-white color-text-grey hover-opacity-off opacity" style="text-decoration: none">
+                fazer login
+            </a>
+        </div>
+        <div class="row clear"><br><br><br><br></div>
     </div>
 
-    <div class="row clear"><br></div>
-    <div class="row upper color-text-grey font-small">
-        <a href="<?= defined('HOME') ? HOME : "" ?>login"
-           class="left btn color-white color-text-grey hover-opacity-off opacity" style="text-decoration: none">
-            fazer login
-        </a>
-    </div>
-    <div class="row clear"><br><br><br><br></div>
-</div>
+<?php
+$data['data'] = ob_get_contents();
+ob_end_clean();
--- ajax/view/esqueci-a-senha.php
+++ ajax/view/esqueci-a-senha.php
@@ -1,24 +1,32 @@
-<div class='row font-large' style="max-width: 450px; margin: auto">
-    <div class="clear"><br><br><br></div>
-    <div class='container align-center upper panel font-light color-text-grey'>Recuperação de Senha</div>
-    <div class="row z-depth-2 color-white">
-        <div class="panel">
+<?php
+ob_start();
+?>
+    <div class='row font-large' style="max-width: 450px; margin: auto">
+        <div class="clear"><br><br><br></div>
+        <div class='container align-center upper panel font-light color-text-grey'>Recuperação de Senha</div>
+        <div class="row z-depth-2 color-white">
+            <div class="panel">
                 <label class="font-small" for="recovery-email">Digite seu Email</label>
                 <input id="recovery-email" type="email">
+            </div>
         </div>
-    </div>
-    <div class="card" style="margin-top: 3px;">
-        <button class="col btn-large color-blue opacity hover-shadow" id="send-email-recover" style="float:initial!important;" onclick="recoveryEmail();">
-            Enviar Email de Recuperação
-        </button>
+        <div class="card" style="margin-top: 3px;">
+            <button class="col btn-large color-blue opacity hover-shadow" id="send-email-recover"
+                    style="float:initial!important;" onclick="recoveryEmail();">
+                Enviar Email de Recuperação
+            </button>
+        </div>
+
+        <div class="row clear"><br></div>
+        <div class="row upper color-text-grey font-small">
+            <a href="<?= defined('HOME') ? HOME : "" ?>login"
+               class="left btn color-white color-text-grey hover-opacity-off opacity" style="text-decoration: none">
+                fazer login
+            </a>
+        </div>
+        <div class="row clear"><br><br><br><br></div>
     </div>
 
-    <div class="row clear"><br></div>
-    <div class="row upper color-text-grey font-small">
-        <a href="<?= defined('HOME') ? HOME : "" ?>login"
-           class="left btn color-white color-text-grey hover-opacity-off opacity" style="text-decoration: none">
-            fazer login
-        </a>
-    </div>
-    <div class="row clear"><br><br><br><br></div>
-</div>
+<?php
+$data['data'] = ob_get_contents();
+ob_end_clean();
