a204582202c7c8bfe3e4d7f3774db1012e3ccb1e
Edinei <galera.org@gmail.com>
2018-10-01 20:13:41 -0300

    update

--- assets/esqueci-a-senha.js
+++ assets/esqueci-a-senha.js
@@ -3,15 +3,20 @@ var recoveryFree = true;
 function recoveryEmail() {
     if (recoveryFree) {
         recoveryFree = false;
-        post('session-control', 'recoveryEmail', {email: $("#recovery-email").val()}, function (g) {
-            if (!g) {
-                $("body").panel(themeNotify('Email não encontrado!', "warning", 4000));
-            }else {
-                $("body").panel(themeNotify('Link de Recuperação enviada ao email', 4000));
-                $("#recovery-email").val("");
-            }
+        let email = $("#recovery-email").val();
+        if(email.length > 6 && email.test(/\w+@\w+.\w+/i)) {
+            post('session-control', 'recoveryEmail', {email: email}, function (g) {
+                if (!g) {
+                    $("body").panel(themeNotify('Email não encontrado!', "warning", 4000));
+                } else {
+                    $("body").panel(themeNotify('Link de Recuperação enviada ao email', 4000));
+                    $("#recovery-email").val("");
+                }
 
-            recoveryFree = true;
-        });
+                recoveryFree = true;
+            });
+        } else {
+            toast("Informe um email válido")
+        }
     }
 }
